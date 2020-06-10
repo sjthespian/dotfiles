13446f190b6285f583c33823eae08ca96b1075f8
Daniel Rich <daniel.rich@disney.com>
2020-06-10 13:43:13 -0700

    Added gcloud includes, including install via. brew

---
+++ bashrc.d/99-gcloud.sh
@@ -0,0 +1,9 @@
+if [ -d /usr/local/google-cloud-sdk ]; then
+  . /usr/local/google-cloud-sdk/completion.bash.inc
+  . /usr/local/google-cloud-sdk/path.bash.inc
+fi
+if [ -d /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk ]; then
+  . /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
+  . /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
+fi
+
