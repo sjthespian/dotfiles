b8918797690a46d44c027ebfe83a282331192654
Daniel Rich <daniel.rich@disney.com>
2020-08-27 10:19:34 -0700

    Added emacs customizations

---
+++ emacs.d/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(case-fold-search t)
+ '(current-language-environment "Latin-1")
+ '(default-input-method "latin-1-prefix")
+ '(focus-follows-mouse t)
+ '(global-font-lock-mode t nil (font-lock))
+ '(load-home-init-file t t)
+ '(package-selected-packages
+   (quote
+    (blacken better-defaults transient transient-dwim forge terraform-mode groovy-mode elpy-enable yaml-mode markdown-mode magit lua-mode json-mode go-mode flycheck-gometalinter dockerfile-mode color-theme-sanityinc-solarized cedit)))
+ '(query-user-mail-address nil)
+ '(user-mail-address "drich@employees.org"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:size "12pt")))))
