f94098c6ac4d92e3fc7082d5613396d74c85e1ef
Daniel Rich <drich@employees.org>
2021-02-13 13:15:03 -0800

    Migrating files from bash to zsh

---
+++ zshrc.d/75-editor.zsh
@@ -0,0 +1,29 @@
+# some editor defaults
+EXINIT='set noautoindent showmatch showmode autowrite redraw'
+export EXINIT
+
+# Set default editor if emacs is available and not forwrding over ssh
+if [ -n "$DISPLAY" ]; then
+    DISPLAYISLOCALHOST=`echo "$DISPLAY" | grep -c localhost`;
+else
+    DISPLAYISLOCALHOST=0
+fi
+# Don't worry about X11 any longer - drich 2020-Feb-07
+#if [ -z "$DISPLAY" -o "$DISPLAYISLOCALHOST" -gt 0 ]; then
+    if `which emacs > /dev/null 2>&1` && `emacs --version > /dev/null 2>&1`; then
+	EDITOR=emacs
+	VISUAL=emacs
+    fi
+#else
+    if `which gnuclient > /dev/null 2>&1` && `gnuclient --batch -f 'emacs-version()' > /dev/null 2>&1`; then
+	EDITOR=gnuclient -t
+	VISUAL=gnuclient
+    fi
+    if `which emacsclient > /dev/null 2>&1` && `emacsclient -e 'emacs-version()' > /dev/null 2>&1`; then
+	export ALTERNATE_EDITOR=""
+	EDITOR="emacsclient -t"
+	VISUAL=emc
+    fi
+#fi
+
+export EDITOR VISUAL
---
+++ zshrc.d/99-misc.zsh
@@ -0,0 +1,88 @@
+# Set the TERM environment variable
+if [ -d /usr/lib/terminfo ]
+then
+    eval `tset -s -Q`
+fi
+
+# Fix bs/del mapping
+stty dec
+stty erase '^H' kill '^U' intr '^C' echoe 
+
+# History management - history by host
+export HISTFILE=$HOME/.zsh_history.`uname -n`
+export HISTIGNORE="&:[bf]g:exit"
+
+# Make less the default pager unless it doesn't exist
+which less > /dev/null 2>&1
+if [ $? == 0 ]; then
+  export PAGER="less -m"
+fi
+
+# Add local fonts
+if [ -n "$DISPLAY" -a -d ~/.fonts ]; then
+  xset +fp ~/.fonts
+fi
+
+# Groovy
+if [ -d /usr/local/opt/groovy/libexec ]; then
+  export GROOVY_HOME=/usr/local/opt/groovy/libexec
+fi
+
+# ChefVM init (https://github.com/trobrock/chefvm)
+if [ -d ~/.chefvm ]; then
+  eval "$(~/.chefvm/bin/chefvm init -)"
+fi
+
+# Android SDK settings from brew
+if [ -d /usr/local/opt/android-sdk ]; then
+  export ANDROID_HOME=/usr/local/opt/android-sdk
+fi
+
+# Python
+if [ -d ~/.virtualenvs -a -e /usr/local/bin/virtualenvwrapper.sh ]; then
+  # Try and find python3
+  for py in /usr/local/bin/python3 /usr/bin/python3; do
+    if [ -e ${py} ]; then
+      export VIRTUALENVWRAPPER_PYTHON=$py
+      break
+    fi
+  done
+  export WORKON_HOME=$HOME/.virtualenvs
+  source /usr/local/bin/virtualenvwrapper.sh
+  export VIRTUAL_ENV_DISABLE_PROMPT=0
+fi
+
+# Go
+which go > /dev/null 2>&1
+if [ $? == 0 ]; then
+  export GOPATH="${HOME}/go/"
+  export PATH=$PATH:$GOPATH/bin
+  # Make sure some common go utils are installed
+  installgoutil() {
+    if [ ! -f $GOPATH/bin/$1 ]; then
+      go get $2
+    fi
+  }
+  installgoutil godef github.com/rogpeppe/godef
+  installgoutil gocode github.com/nsf/gocode
+  installgoutil goimports golang.org/x/tools/cmd/goimports
+  installgoutil guru golang.org/x/tools/cmd/guru
+fi
+
+# JAVA_HOME settings on the Mac
+if [ -d /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/ ]; then
+  export JAVA_HOME=/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/
+fi
+
+# Syntax highlighting in less on OSX
+# http://funkworks.blogspot.com/2013/01/syntax-highlighting-in-less-on-osx.html
+if `hash src-hilite-lesspipe.sh > /dev/null 2>&1`; then
+  LESSPIPE=`which src-hilite-lesspipe.sh`
+  if [ -n "$LESSPIPE" ]; then
+    export LESSOPEN="| ${LESSPIPE} %s"
+    export LESS='-R'
+  fi
+fi
+
+# Postfix aliases
+alias cleanqueue="mailq| grep MAILER-DAEMON| awk '{print $1}' | sed 's/\*$//' | xargs -r -n1 sudo postsuper -d"
